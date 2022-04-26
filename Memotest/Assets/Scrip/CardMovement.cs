ba23754c09da61c0a71d50a4aed17ab760b874e7
aula <martinconcetti97@gmail.com>
2022-04-26 11:35:57 -0300

    Card prefab rotates

---
+++ Memotest/Assets/Scrip/CardMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardMovement : MonoBehaviour
+{
+    [SerializeField] private float reveal;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnMouseUp()
+    {
+        Debug.Log("Fuck");
+        transform.Rotate(new Vector3(0, 0, reveal) /** Time.deltaTime*/);
+    }
+}
